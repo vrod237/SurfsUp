a33498e373f9ce776a1e556e74e9ab4499aba300
Victor Rodriguez IV <65783896+vrod237@users.noreply.github.com>
2020-08-02 23:25:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func, extract
+from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
 #%%
